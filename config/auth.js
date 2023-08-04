99cbac922824fc4afe70715fa0352697f4ea2199
SreeChakram-004 <cherrytheraider@gmail.com>
2023-08-04 21:08:30 +0530

    backend code deployed

--- config/auth.js
+++ config/auth.js
@@ -4,7 +4,7 @@ module.exports = {
     googleAuth: {
       clientID: '515654190396-vo2astd2tvmccrndihbq1cpbp9rj6lal.apps.googleusercontent.com',
       clientSecret: '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',
-      callbackURL: 'http://localhost:5000/auth/google/callback',
+      callbackURL: 'https://cyan-good-chiton.cyclic.app/auth/google/callback',
     },
     jwtSecret: 'YOUR_JWT_SECRET', // Replace with your JWT secret key
   };
