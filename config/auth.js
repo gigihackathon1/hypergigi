3cf1d24cf415efcdafd9cd5eb0804866d5721acb
SreeChakram-004 <cherrytheraider@gmail.com>
2023-08-04 23:45:18 +0530

    google credentials changed

--- config/auth.js
+++ config/auth.js
@@ -2,8 +2,8 @@
 
 module.exports = {
     googleAuth: {
-      clientID: '515654190396-vo2astd2tvmccrndihbq1cpbp9rj6lal.apps.googleusercontent.com',
-      clientSecret: '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',
+      clientID: '515654190396-2tc0ma3ibafpeergispdmqrivifirp7o.apps.googleusercontent.com',
+      clientSecret: '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',
       callbackURL: 'https://cyan-good-chiton.cyclic.app/auth/google/callback',
     },
     jwtSecret: 'YOUR_JWT_SECRET', // Replace with your JWT secret key
