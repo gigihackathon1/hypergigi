9e26444aa82cd33245e625bef194154efb65886f
SreeChakram-004 <cherrytheraider@gmail.com>
2023-08-05 16:54:27 +0530

    cors error fixed

--- config/passport.js
+++ config/passport.js
@@ -1,5 +1,5 @@
 const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth2').Strategy;
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const { googleAuth, jwtSecret } = require('./auth');
 const { User } = require('../models');
 const bcrypt = require('bcryptjs');
