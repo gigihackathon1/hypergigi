c19b6cb8c605475082c4bf7c0c3afc1d42b600ca
SreeChakram-004 <cherrytheraider@gmail.com>
2023-08-05 17:25:43 +0530

    new google credentials

--- config/auth.js
+++ config/auth.js
@@ -2,9 +2,9 @@
 
 module.exports = {
     googleAuth: {
-      clientID: '515654190396-2tc0ma3ibafpeergispdmqrivifirp7o.apps.googleusercontent.com',
-      clientSecret: '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',
-      callbackURL: ' https://cyan-good-chiton.cyclic.app/auth/google/callback',
+      clientID: '515654190396-fi8ogeihr1l85mtatcm5fhr1i4tdsmku.apps.googleusercontent.com',
+      clientSecret: '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',
+      callbackURL: 'https://cyan-good-chiton.cyclic.app/auth/google/callback',
     },
     jwtSecret: 'YOUR_JWT_SECRET', // Replace with your JWT secret key
   };
